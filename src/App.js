fde64a7d231bf1083b0ede04e5e3011bb386b3bc
jdvolk <justin.d.volk@gmail.com>
2021-05-15 16:21:26 -0600

    app structure and pulling in menu component/hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import MenuPage from './Components/Menu';
+import video from './assets/ink4k.mov';
+import poster from './assets/poster.jpg';
+import UseMobilePoster from "./Hooks/UseMobilePoster"
+// import MenuPage from './Components/MenuDynamic';
+// import { useQuery } from "react-query";
 
 function App() {
+  //todo create function to dectect screen size and output wither a video or a poster
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {window.innerWidth && UseMobilePoster(video,poster)}
+    {/* <video className="video" playsInline autoPlay muted loop poster={poster} srcmob={poster}> <source src={video} type='video/mp4' /> </video>  */}
+        <MenuPage />
       </header>
     </div>
   );
